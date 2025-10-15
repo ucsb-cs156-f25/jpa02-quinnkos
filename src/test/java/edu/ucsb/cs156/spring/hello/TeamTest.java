1b40526f3b7565f2fdf9d9c3eb93876b9995332a
Quinn Koster <qtrk05@gmail.com>
2025-10-14 21:53:42 -0700

    xy - added team tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +20,41 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void team_equals_team() {
+        assertEquals(team, team);
+    }
+
+    @Test
+    public void team_equals_different_class() {
+        assertFalse(team.equals("not a team"));
+    }
+
+    @Test
+    public void team_equals_same_name_different_members() {
+        Team team1 = new Team("my_team");
+        Team team2 = new Team("my_team");
+        team2.addMember("Bob the Builder");
+        assertFalse(team1.equals(team2));
+    }
+
+    @Test
+    public void team_equals_different_name_same_members() {
+        Team team1 = new Team("team1");
+        Team team2 = new Team("team2");
+        assertFalse(team1.equals(team2));
+    }
+
+    @Test
+    public void team_hash_equals_team_hash() {
+        Team t = new Team("test");
+        int result = t.hashCode();
+        int expectedResult = 3556499;
+        assertEquals(expectedResult, result);
+    }
 }
